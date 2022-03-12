ab67fc080f74b31d9d86e91999c2b467537c20e6
dragoonoid <20bec044@iiitdmj.ac.in>
2022-03-13 02:32:44 +0530

    1000

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
+  int _counter = 1000;
 
   void _incrementCounter() {
     setState(() {
